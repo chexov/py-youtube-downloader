22a12120715b3785a21dcae6e41dc342e391ae7f
dbr <dbr.onix@gmail.com>
2010-02-09 22:57:20 +0000

    Test title retrival

--- test_youtube.py
+++ test_youtube.py
@@ -3,9 +3,18 @@
 
 import youtube as y
 def test_getPageToken():
+    """Tests pageToken method returns sensible string
+    """
     token = y.Youtube("QkzCi5mHvkc").pageToken()
     assert isinstance(token, basestring)
     assert len(token) > 0, "Token was zero length"
+
+def test_getPageTitle():
+    """Tests retrieval of page title
+    """
+    token = y.Youtube("QkzCi5mHvkc").title()
+    print token
+    assert False
 
 if __name__ == '__main__':
     import nose
